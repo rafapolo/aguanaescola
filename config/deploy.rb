171d304fe3d1df2a4c459713cb2e095a48b5874d
Rafael Polo <rafael.polo@gmail.com>
2013-03-19 17:46:00 -0300

    ajuste em logos

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ role :app, 'agendaaguanaescola.eco.br'
 
 namespace :deploy do
   task :restart, :roles => :app do
+  	run "#{try_sudo} bundle install --deployment"
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
