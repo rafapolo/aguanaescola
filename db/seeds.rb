346810198bec286bae734f77f846d6ffe555c785
Rafael Polo <rafael.polo@gmail.com>
2013-03-20 13:40:58 -0300

    logos mais altas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-e = Escola.create(:nome=>"Escola Cantagalgo", :municipio=>"Nova Friburgo")
+e = Escola.create(:nome=>"Escola Cantagalo", :municipio=>"Nova Friburgo")
 p = Pessoa.create(:nome=>"Polo Admin", :senha=>"12345", :email=>"rafael.polo@gmail.com", :escola_id => e.id)
 p.update_attribute(:is_admin, true)
 
