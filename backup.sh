09e5873acde0d8e9ad855c908713de9ed2c5b3bb
Rafael Polo <rafael.polo@gmail.com>
2013-11-27 22:07:48 +0100

    fix local backup script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e "\033[33;44m AgendaAgua | Backing up database...\033[0m"
-rsync -avz agua@agendaaguanaescola.eco.br:~/agendaaguanaescola.eco.br/shared/db/prod.sqlite3 ~/Projetos/Agenda\ Água\ na\ Escola/aguanaescola/db/dev.sqlite3
+rsync -avz agua@agendaaguanaescola.eco.br:~/agendaaguanaescola.eco.br/shared/db/prod.sqlite3 ~/Projetos/Agenda\ Agua\ na\ Escola/aguanaescola/db/dev.sqlite3
 echo -e "\033[33;44m AgendaAgua | Backing up uploads...\033[0m"
-rsync -ravz agua@agendaaguanaescola.eco.br:~/agendaaguanaescola.eco.br/shared/uploads/ ~/Projetos/Agenda\ Água\ na\ Escola/aguanaescola/public/uploads/
+rsync -ravz agua@agendaaguanaescola.eco.br:~/agendaaguanaescola.eco.br/shared/uploads/ ~/Projetos/Agenda\ Agua\ na\ Escola/aguanaescola/public/uploads/
