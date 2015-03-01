ad6e4661121b0a573b24d0d700dab8850d5bc669
Rafael Polo <rafael.polo@gmail.com>
2015-03-01 16:29:40 +0100

    aumenta segurança

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,18 @@
 class AdminController < ApplicationController
   before_filter :count
-  
+
   def count
     @coletas_count = Coleta.count
     @escolas_count = Escola.count
     @monitores_count = Pessoa.monitores.count
+    redirect_to '/login' unless current_user
   end
 
   def index
-  	admin = current_user && current_user.is_monitor? ? "/admin/coletas" : "/admin/monitores"
+  	admin = current_user.is_monitor? ? "/admin/coletas" : "/admin/monitores"
     redirect_to admin
   end
-  
+
   def escolas
   	@escolas = Escola.all
   end
